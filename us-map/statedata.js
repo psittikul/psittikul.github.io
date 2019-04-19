25bd804b299cb74feb3d5f70f99f4e01c6fda042
psittikul <patricia.sitt@gmail.com>
2019-04-19 02:39:27 -0400

    Job map fun

---
+++ us-map/statedata.js
@@ -0,0 +1,104 @@
+var stateData = [
+    {
+        state: 'TX',
+        label: 'Austin, TX',
+        population: 950715,
+        col_index: 130,
+        companies: ['Google', 'Samsung', 'Bumble', 'NTTData', 'UT Austin', 'SolarWinds'],
+        pros: ["#2 Best City for Tech", "Warm???", "Pretty hip?", '"Keep Austin Weird"'],
+        cons: ["It's Texas", "It's Texas", "Racism???", "No Wawa"]
+    },
+    {
+        state: 'NY',
+        label: 'New York, NY',
+        population: 8600000,
+        col_index: 209,
+        companies: ['Spotify', 'NYTimes', 'NRDC', 'Google', 'Palantir', 'Paladin', 'TED'],
+        pros: ['Close to family', 'Could commute from LV', "It's NYC"],
+        cons: ["It's NYC", "Commute would probably suck", "Expensive AF", "It's NYC"]
+    },
+    {
+        state: 'GA',
+        label: 'Atlanta, GA',
+        population: 900000,
+        col_index: 120,
+        companies: ['Adult Swim', 'Spotify'],
+        pros: ['Hip?', 'Warm'],
+        cons: ['The South???', 'Traffic is terrible', 'WTF is Georgia']
+    },
+    {
+        state: 'WA',
+        label: 'Seattle, WA',
+        population: 1000000,
+        col_index: 130,
+        companies: ['Atlassian', 'Spotify', 'Google', 'Nintendo'],
+        pros: ['Hip', 'Pacific Northwest', 'Progressive'],
+        cons: ['So fucking far']
+    },
+    {
+        state: 'CA',
+        label: 'San Francisco, CA',
+        population: 1200000,
+        col_index: 200,
+        companies: ['Spotify', 'Atlassian', 'Palantir'],
+        pros: ['West Coast', 'Hip', 'Warm', 'Beaches', 'Progressive'],
+        cons: ['CA seems kinda mainstream idk man', 'Natural disasters???', 'Expensive AF']
+    },
+    {
+        state: 'PA',
+        label: '',
+        population: 400000,
+        col_index: 90,
+        companies: ['Duolingo', 'Spotify?', 'Hearst'],
+        pros: ['Close to family', 'Wawa', 'Could stay in LV', 'Nature', 'Affordable', 'You love Bethlehem'],
+        cons: ["You're bored", 'You should get out into the world even if it scares you']
+    },
+    {
+        state: 'NC',
+        label: 'Chapel Hill, NC',
+        population: 1000000,
+        col_index: 140,
+        companies: ['Uhh lol'],
+        pros: ['Warm?', 'Beaches', 'Hip?', '#1 Best City for Tech'],
+        cons: ['WTF is in North Carolina', 'South of Mason Dixon line', 'Conservatives???']
+    }
+]
+
+
+var statesOfInterest = ['TX', 'NY'];
+
+var contacts = [
+    {
+        contact: 'Amber Wallace',
+        company: 'Atlassian',
+        status: 'No Connection Yet',
+        source: 'Theta'
+    },
+    {
+        contact: 'Pete Matt',
+        company: 'Google',
+        status: 'Connected',
+        source: 'CSE 216'
+    },
+    {
+        contact: 'Name Here',
+        company: 'Palantir',
+        status: 'No Connection Yet',
+        source: 'LinkedIn'
+    }
+]
+
+var jobs = [
+    {
+        company: 'Company Name',
+        position: 'Full-Stack Developer', 
+        deadline: 'idk',
+        salary: 75000,
+        skills: 'JavaScript',
+        ranking: 2,
+        url: 'www.jobpostingurl.com'
+    }
+];
+
+var companies = ["Google", "Spotify", "NRDC", "Bumble", "Nintendo", "Palantir", "NY Times", "Parse.ly",
+                 "That Progressive News thing Baumer told you about", "ACLU"]
