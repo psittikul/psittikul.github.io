b27836fd9e34062a1505db5c49310adf28151eac
psittikul <patricia.sitt@gmail.com>
2019-05-11 12:48:01 -0400

    Updates to front page; added portfolio page and a data file

---
+++ data/projects.js
@@ -0,0 +1,23 @@
+var projects = [
+    {
+        id: 0,
+        title: 'My Job Map',
+        website: 'https://psittikul.github.io/myjobmap/jobmap.html',
+        hash: 'myjobmap',
+        last_update: '05/11/2019'
+    },
+    {
+        id: 1,
+        title: 'Topicalizer Autism Blogs',
+        website: 'http://das-lab.org:8000/',
+        hash: 'topicalizer',
+        last_update: '04/30/2019'
+    },
+    {
+        id: 2,
+        title: 'PodPals',
+        website: 'podpals.hopto.org',
+        hash: 'podpals',
+        last_update: '12/05/2018'
+    }
+];
