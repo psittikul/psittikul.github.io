073461beb7d0efe85503b83b9cedafbf3c6a1a7f
Patricia Sittikul <pas217@lehigh.edu>
2020-09-10 18:20:45 -0400

    Updated about page, started portfolio page, implementing Turn.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
-$(function() {
+$(function () {
     $("[data-toggle='tooltip']").tooltip();
-})
+    // Add loaded class to body for cool transitions
+    $("body").addClass("loaded");
+
+    $.fn.isInViewport = function () {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        console.log("Element top: " + elementTop + " Element bottom: " + elementBottom + " Viewport top: " + viewportTop + " Viewport bottom/2: " + viewportBottom / 2);
+        return elementBottom > viewportTop && elementTop < (viewportBottom / 1.25);
+    };
+    $(window).on('resize scroll', function () {
+        // Don't do anything if there isn't actually a carousel on the page
+        if ($(".carousel").length < 1) {
+            return;
+        }
+        if ($("#latestWorkCarousel").isInViewport()) {
+            console.log("At least half of carousel is in viewport?!");
+            $("#latestWorkCarousel").removeClass("out-of-focus");
+        }
+        else {
+            console.log("Carousel is mostly out of viewport?");
+            $("#latestWorkCarousel").addClass("out-of-focus");
+        }
+    });
+    $("#flipbook").turn({
+        width: 400,
+        height: 300,
+        autoCenter: true
+    });
+});
